2dcf01a1d53e52248e3a30a51721beea3955769a
Dino Tudor <dinotudor@gmail.com>
2020-05-28 23:32:34 -0300

    UPDATE WIP

--- src/app.js
+++ src/app.js
@@ -31,7 +31,21 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+    const repoIndex = repositories.findIndex(respository => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  const updated = { id, url, title, techs };
+
+  repositories[repoIndex] = updated;
+
+  return response.status(200).json(updated);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
